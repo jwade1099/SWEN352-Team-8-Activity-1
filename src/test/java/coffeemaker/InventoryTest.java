64699fe72280bc2d8856b2358144e77097669981
Justin <jpwade109@gmail.com>
2025-01-28 15:11:52 -0500

    inventory test

--- src/test/java/coffeemaker/InventoryTest.java
+++ src/test/java/coffeemaker/InventoryTest.java
@@ -20,13 +20,8 @@ public class InventoryTest {
 		inventory = new Inventory();
 	}
 
-	// @AfterEach
-	// public void tearDown() {
-	
-	// }
-
 	@Test
-	 void tesdtEmptyInventory() {
+	 void testEmptyInventory() {
         assertNotNull(inventory, "Inventory should be created");
         assertEquals(0, inventory.getCoffee(), "Default coffee should be 0");
         assertEquals(0, inventory.getMilk(), "Default milk should be 0");
@@ -45,6 +40,25 @@ public class InventoryTest {
         assertEquals(3, inventory.getMilk(), "Milk should be 3");
         assertEquals(8, inventory.getSugar(), "Sugar should be 8");
         assertEquals(3, inventory.getChocolate(), "Chocolate should be 3");
+    }
+
+    @AfterEach
+    public void tearDown() {
+		inventory = null;
+	}
+
+
+    @Test
+    void testSetIngredients() {
+        inventory.setCoffee(5);
+        inventory.setMilk(7);
+        inventory.setSugar(9);
+        inventory.setChocolate(11);
+
+        assertEquals(5, inventory.getCoffee(), "Coffee should be 5");
+        assertEquals(7, inventory.getMilk(), "Milk should be 7");
+        assertEquals(9, inventory.getSugar(), "Sugar should be 9");
+        assertEquals(11, inventory.getChocolate(), "Chocolate should be 11 yummy");
     }
 	
 }
