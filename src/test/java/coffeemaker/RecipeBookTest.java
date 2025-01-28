304a7666648b9020fc7ca26cdf9452383adbde41
Aidan Call <acc9123@rit.edu>
2025-01-28 15:06:11 -0500

    recipebooktest base file

---
+++ src/test/java/coffeemaker/RecipeBookTest.java
@@ -0,0 +1,22 @@
+package coffeemaker;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class RecipeBookTest {
+    @BeforeEach
+	public void setUp() {
+		// add setup here
+//		CuT = new CoffeeMaker();
+	}
+
+	@AfterEach
+	public void tearDown() {
+		// add tear down here
+//		CuT = null;
+	}
+}
